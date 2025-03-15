c27a193fb86aa814c423483928aff20f3b170769
Dany <Dany@DESKTOP-LR3MLCS>
2025-03-15 20:13:01 +0330

    Add project files.

---
+++ README.txt
@@ -0,0 +1,13 @@
+CMake Toturial :  
+
+1. cmake_minimum_required (VERSION x...y) # x to y version
+2. project (project_name)
+3. add_executable project_exe_name "x.cpp" "y.h ..." )
+4. set(CMAKE_CXX_STANDARD xx)
+5. set(CMAKE_CXX_STANDARD_REQUIRED True)
+6. project(project_name VERSION 1.0) # project version control 
+7. 
+
+
+advance : 
+1. configure_file(input.in output)
---
+++ src/RSSReaderSmartAssist.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <curl/curl.h>
+#include "RSSReaderSmartAssistConfig"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		// report version
+		cout << argv[0] << " Version " << RSSReaderSmartAssist_VERSION_MAJOR
+			<< "." << RSSReaderSmartAssist_VERSION_MINOR << endl;
+	}
+
+	CURL* curl;
+	curl = curl_easy_init();
+	if (curl == nullptr)
+	{
+		cout << "error in curl_easy_init" << endl;
+		Sleep(3000);
+		exit(EXIT_FAILURE);
+	}
+
+
+	cout << "Hello CMake." << endl;
+	return 0;
+}
